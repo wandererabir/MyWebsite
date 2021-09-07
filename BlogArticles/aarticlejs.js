8b9aefb33e685d18cc5f1835043bb134f23dd5b7
ABIR PAUL <65394557+wandererabir@users.noreply.github.com>
2021-09-07 19:29:03 +0530

    updated share buttons

--- BlogArticles/aarticlejs.js
+++ BlogArticles/aarticlejs.js
@@ -1,3 +1,37 @@
 window.onload = function() {
     document.body.className += ' loaded'
   };
+
+
+const facebookBtn = document.querySelector(".facebook-btn");
+const twitterBtn = document.querySelector(".twitter-btn");
+const linkedinBtn = document.querySelector(".linkedin-btn");
+const whatsappBtn = document.querySelector(".whatsapp-btn");
+  
+  function init() {
+    let postUrl = encodeURI(document.location.href);
+    let postTitle = encodeURI("Check out this blog, do share your feedbacks : ");
+
+  
+    facebookBtn.setAttribute(
+      "href",
+      `https://www.facebook.com/sharer.php?u=${postUrl}`
+    );
+  
+    twitterBtn.setAttribute(
+      "href",
+      `https://twitter.com/share?url=${postUrl}&text=${postTitle}`
+    );
+  
+    linkedinBtn.setAttribute(
+      "href",
+      `https://www.linkedin.com/shareArticle?url=${postUrl}&title=${postTitle}`
+    );
+  
+    whatsappBtn.setAttribute(
+      "href",
+      `https://wa.me/?text=${postTitle} ${postUrl}`
+    );
+  }
+  
+init(); 
